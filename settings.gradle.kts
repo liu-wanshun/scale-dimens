5a6fcd5b32cdc69f3a8ed7232a74f42d7a33bc05
liu_wanshun <liu_wanshun@qq.com>
2025-09-16 02:01:24 +0800

    chore: backward compatibility supports JDK11

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,9 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-//        maven {
-//            url = uri("https://www.jitpack.io/")
-//        }
+        maven {
+            url = uri("https://www.jitpack.io/")
+        }
     }
 }
 
