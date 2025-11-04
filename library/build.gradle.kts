4a04956e0344e3bcf4385374881f02efca099322
无言 <57122860+liu-wanshun@users.noreply.github.com>
2025-11-04 15:56:27 +0800

    Disable configPath and println in scaleDimens
    Comment out configPath and println statements in scaleDimens.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -29,6 +29,6 @@ android {
 scaleDimens {
     baseSw = 360
     generateSwList = intArrayOf(360, 720)
-    configPath = rootProject.file("scale-demens.yaml").absolutePath
-    println("configPath = $configPath")
+    //configPath = rootProject.file("scale-demens.yaml").absolutePath
+    //println("configPath = $configPath")
 }
